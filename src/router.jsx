f064fee32dd08c8ec60a5736005debe6f45fdc7d
laural62 <laisne.laura62@gmail.com>
2025-07-28 11:22:58 +0200

    modification du 28 juillet onze heure

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/Login";
+import Header from "./components/Header";
 
 export const routerTest = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const routerTest = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path: "/header",
+        element: <Header />,
       },
     ],
   },
